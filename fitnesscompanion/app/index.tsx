eadb09db26d2bc397ffc58a354f09a41488b4a94
AhmedTMM <52082882+AhmedTMM@users.noreply.github.com>
2024-06-13 11:57:32 -0700

    button

--- fitnesscompanion/app/index.tsx
+++ fitnesscompanion/app/index.tsx
@@ -1,15 +1,43 @@
-import { Text, View } from "react-native";
+// App.js
+import React from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity,
+  Text,
+} from 'react-native';
 
-export default function Index() {
-  return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
-  );
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity style={styles.button} onPress={console.log("Woah")}>
+          <Text style={styles.buttonText}>Connect</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    backgroundColor: 'white',
+    padding: 20,
+    width: "70%",
+    height: "33%",
+    alignItems: "center",
+    justifyContent: "center",
+    borderRadius: 20,
+  },
+  buttonText: {
+    color: 'black',
+    fontSize: 60,
+  },
+});
