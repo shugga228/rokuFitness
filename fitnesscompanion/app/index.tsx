e8e18cbd7202c95440e2bc7781f372b776b8e04a
AhmedTMM <52082882+AhmedTMM@users.noreply.github.com>
2024-06-14 09:01:05 -0700

    added images

--- fitnesscompanion/app/index.tsx
+++ fitnesscompanion/app/index.tsx
@@ -1,20 +1,22 @@
-// App.js
 import React from 'react';
 import {
   StyleSheet,
   View,
   TouchableOpacity,
   Text,
+  Image,
 } from 'react-native';
-
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <TouchableOpacity style={styles.button} onPress={console.log("Woah")}>
+                <Image source={require('../assets/images/companionapp.png')} style={styles.image} />
+
+        <TouchableOpacity style={styles.button} onPress={() => console.log("Woah")}>
           <Text style={styles.buttonText}>Connect</Text>
         </TouchableOpacity>
+        <Image source={require('../assets/images/companionapp.png')} style={styles.image} />
       </View>
     );
   }
@@ -24,14 +26,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: 'black',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
     alignItems: 'center',
   },
   button: {
     backgroundColor: 'white',
     padding: 20,
-    width: "70%",
-    height: "33%",
+    width: "80%",
+    height: "20%",
     alignItems: "center",
     justifyContent: "center",
     borderRadius: 20,
@@ -39,5 +41,10 @@ const styles = StyleSheet.create({
   buttonText: {
     color: 'black',
     fontSize: 60,
+  },
+  image: {
+    width: "80%",  // Adjust the width as per your requirement
+    height: "20%", // Adjust the height as per your requirement
+    margin: 10,
   },
 });
