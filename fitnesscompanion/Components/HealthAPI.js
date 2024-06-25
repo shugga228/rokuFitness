a537df0529f8b64fe04eef955befe4e6066a4b77
AhmedTMM <52082882+AhmedTMM@users.noreply.github.com>
2024-06-25 14:32:32 -0700

    HealthAPI
    Only for android right now and is untested.  Will test/implement tomorrow.

--- fitnesscompanion/Components/HealthAPI.js
+++ fitnesscompanion/Components/HealthAPI.js
@@ -1,5 +1,12 @@
 import AppleHealthKit from 'react-native-health';
 import { Platform } from 'react-native';
+import {
+    initialize,
+    requestPermission,
+    readRecords,
+    readRecord,
+  } from 'react-native-health-connect';
+  
 
 /* Permission options */
 const permissions = {
@@ -9,31 +16,69 @@ const permissions = {
   },
 };
 
-export const APIInit = () => {
+export const APIInit = async () => {
     if (Platform.OS === 'ios') {
-        // do something for ios
+        AppleHealthKit.initHealthKit(permissions, (err, results) => {
+            if (err) {
+              console.log('error initializing Healthkit: ', err);
+              return;
+            }
+            // Healthkit is initialized...
+            // now safe to read and write Healthkit data...
+        
+            const options = {
+              startDate: new Date(2020, 1, 1).toISOString(),
+            };
+        
+            AppleHealthKit.getHeartRateSamples(options, (callbackError, results) => {
+              if (callbackError) {
+                console.log('[ERROR] Failed to fetch heart rate samples!', callbackError);
+                return;
+              }
+        
+              console.log('Heart Rate Samples:', results);
+            });
+          });
       } else if (Platform.OS === 'android') {
-        // other thing for android
+        await initialize();
+        const grantedPermissions = await requestPermission([
+            { accessType: 'read', recordType: 'ActiveCaloriesBurned' },
+            { accessType: 'read', recordType: 'Weight' },
+            { accessType: 'read', recordType: 'Height' },
+          ]);        
       }      
-  AppleHealthKit.initHealthKit(permissions, (err, results) => {
-    if (err) {
-      console.log('error initializing Healthkit: ', err);
-      return;
-    }
-    // Healthkit is initialized...
-    // now safe to read and write Healthkit data...
-
-    const options = {
-      startDate: new Date(2020, 1, 1).toISOString(),
-    };
-
-    AppleHealthKit.getHeartRateSamples(options, (callbackError, results) => {
-      if (callbackError) {
-        console.log('[ERROR] Failed to fetch heart rate samples!', callbackError);
-        return;
-      }
-
-      console.log('Heart Rate Samples:', results);
-    });
-  });
 };
+export const Calories = async () => {
+    const startTime = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 0, 0, 0, 0);
+    if (Platform.OS === "android") {
+        const isInitialized = await initialize();
+        if (isInitialized) {
+            const result = await readRecords('ActiveCaloriesBurned', {
+                timeRangeFilter: {
+                operator: 'between',
+                startTime: startTime.toISOString(),
+                endTime: new Date.toISOString(),
+                },
+            });
+            return result;        
+        }
+    }
+}
+export const Weight = async () => {
+    if (Platform.OS === "android") {
+        const isInitialized = await initialize();
+        if (isInitialized) {
+            const result = await readRecord('Weight');
+            return result;        
+        }
+    }
+}
+export const Height = async () => {
+    if (Platform.OS === "android") {
+        const isInitialized = await initialize();
+        if (isInitialized) {
+            const result = await readRecord('Height');
+            return result;        
+        }
+    }
+}
